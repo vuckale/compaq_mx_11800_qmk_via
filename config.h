38eade83a64922efbc977e253f7faa5f515cb28c
vuckale <56733276+vuckale@users.noreply.github.com>
2021-08-08 23:53:29 +0200

    add audio pin and PS/2 trackball settings

--- config.h
+++ config.h
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include "config_common.h"
+#include "song_list.h"
 
 #define VENDOR_ID       0x049F
 #define PRODUCT_ID      0xB944
@@ -31,33 +32,76 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 16
 
-#define MATRIX_ROW_PINS {F7, F6, F5, F4, F3, F2, F1, F0}
-#define MATRIX_COL_PINS { C7, C6, C5, C4, C3, C2, C1, C0, E1, E0, D7, D6, E6, E7, B0, B1 }
-#define UNUSED_PINS { B4, B5, B6, D5, D4, D3, D2, D1, D0, B7, A7, A6, A5, A4, A4, A2, A1, A0, E4, E3}
+#define MATRIX_ROW_PINS { C0, C1, C2, C3, C4, C5, C6, C7 }
+#define MATRIX_COL_PINS { F7, F6, F5, F4, F3, F2, F1, F0, E6, E7, B0, B1, B2, B3, B4, B5 }
+#define UNUSED_PINS { B6, D6, D4, D3, D1, D0, A7, A6, A5, A4, A4, A2, A1, A0, E4, E3}
 
-#define LED_SCROLL_LOCK_PIN B2
-#define LED_CAPS_LOCK_PIN B3
-#define LED_NUM_LOCK_PIN B4
+#def                                                     ine LED_SCROLL_LOCK_PIN E1
+#define LED_CAPS_LOCK_PIN E0
+#define LED_NUM_LOCK_PIN D7
 
 #define DIODE_DIRECTION COL2ROW
 
 // Set 0 if debouncing isn't needed
 #define DEBOUNCE 5
 
-// Define layers count
 #ifndef DYNAMIC_KEYMAP_LAYER_COUNT
 #    define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #endif
 
-// Define keymap macro count
 #ifndef DYNAMIC_KEYMAP_MACRO_COUNT
 #    define DYNAMIC_KEYMAP_MACRO_COUNT 16
 #endif
 
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 4095
 
-//Define bootmagic on ESC
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 13
+
+#define PS2_MOUSE_USE_2_1_SCALING
+
+
+#ifdef PS2_USE_USART
+#    define PS2_CLOCK_PORT PORTD
+#    define PS2_CLOCK_PIN PIND
+#    define PS2_CLOCK_DDR DDRD
+#    define PS2_CLOCK_BIT 5
+#    define PS2_DATA_PORT PORTD
+#    define PS2_DATA_PIN PIND
+#    define PS2_DATA_DDR DDRD
+#    define PS2_DATA_BIT 2
+
+#    define PS2_USART_INIT()                                                                         \
+        do {                                                                                         \
+            PS2_CLOCK_DDR &= ~(1 << PS2_CLOCK_BIT);                                                  \
+            PS2_DATA_DDR &= ~(1 << PS2_DATA_BIT);                                                    \
+            UCSR1C = ((1 << UMSEL10) | (3 << UPM10) | (0 << USBS1) | (3 << UCSZ10) | (0 << UCPOL1)); \
+            UCSR1A = 0;                                                                              \
+            UBRR1H = 0;                                                                              \
+            UBRR1L = 0;                                                                              \
+        } while (0)
+#    define PS2_USART_RX_INT_ON()                    \
+        do {                                         \
+            UCSR1B = ((1 << RXCIE1) | (1 << RXEN1)); \
+        } while (0)
+#    define PS2_USART_RX_POLL_ON() \
+        do {                       \
+            UCSR1B = (1 << RXEN1); \
+        } while (0)
+#    define PS2_USART_OFF()                           \
+        do {                                          \
+            UCSR1C = 0;                               \
+            UCSR1B &= ~((1 << RXEN1) | (1 << TXEN1)); \
+        } while (0)
+#    define PS2_USART_RX_READY (UCSR1A & (1 << RXC1))
+#    define PS2_USART_RX_DATA UDR1
+#    define PS2_USART_ERROR (UCSR1A & ((1 << FE1) | (1 << DOR1) | (1 << UPE1)))
+#    define PS2_USART_RX_VECT USART1_RX_vect
+#endif
+
+#define AUDIO_PIN_ALT B7
+#ifdef AUDIO_ENABLE
+#    define STARTUP_SONG SONG(STARTUP_SOUND)
+#endif
 
 #define MATRIX_HAS_GHOST
