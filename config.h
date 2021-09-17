8aedeab18bb5554abe11db7a6f1aacb3cd9ab669
vuckale <56733276+vuckale@users.noreply.github.com>
2021-09-17 22:59:27 +0200

    cleanup config files

--- config.h
+++ config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0xB944
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Compaq Computer Comporation
-#define PRODUCT         COMPAQ MX11800
-#define DESCRIPTION     Server Keyboard
+#define PRODUCT         Compaq MX 11800
+#define DESCRIPTION     Server Rack Keyboard
 
 #define CORE_TEENSY
 
@@ -45,65 +45,49 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Set 0 if debouncing isn't needed
 #define DEBOUNCE 5
 
-#ifndef DYNAMIC_KEYMAP_LAYER_COUNT
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#endif
-
-#ifndef DYNAMIC_KEYMAP_MACRO_COUNT
-#    define DYNAMIC_KEYMAP_MACRO_COUNT 16
-#endif
-
-#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 4095
-
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 13
 
-
-#define PS2_MOUSE_USE_2_1_SCALING
-
-#define PS2_MOUSE_X_MULTIPLIER 4
-#define PS2_MOUSE_Y_MULTIPLIER 4
-#define PS2_MOUSE_V_MULTIPLIER 2
-
-#define PS2_MOUSE_SCROLL_DIVISOR_H 4
-#define PS2_MOUSE_SCROLL_DIVISOR_V 4
-
 #ifdef PS2_USE_USART
-#    define PS2_CLOCK_PORT PORTD
-#    define PS2_CLOCK_PIN PIND
-#    define PS2_CLOCK_DDR DDRD
-#    define PS2_CLOCK_BIT 5
-#    define PS2_DATA_PORT PORTD
-#    define PS2_DATA_PIN PIND
-#    define PS2_DATA_DDR DDRD
-#    define PS2_DATA_BIT 2
+#define PS2_CLOCK_PORT  PORTD
+#define PS2_CLOCK_PIN   PIND
+#define PS2_CLOCK_DDR   DDRD
+#define PS2_CLOCK_BIT   5
+#define PS2_DATA_PORT   PORTD
+#define PS2_DATA_PIN    PIND
+#define PS2_DATA_DDR    DDRD
+#define PS2_DATA_BIT    2
 
-#    define PS2_USART_INIT()                                                                         \
-        do {                                                                                         \
-            PS2_CLOCK_DDR &= ~(1 << PS2_CLOCK_BIT);                                                  \
-            PS2_DATA_DDR &= ~(1 << PS2_DATA_BIT);                                                    \
-            UCSR1C = ((1 << UMSEL10) | (3 << UPM10) | (0 << USBS1) | (3 << UCSZ10) | (0 << UCPOL1)); \
-            UCSR1A = 0;                                                                              \
-            UBRR1H = 0;                                                                              \
-            UBRR1L = 0;                                                                              \
-        } while (0)
-#    define PS2_USART_RX_INT_ON()                    \
-        do {                                         \
-            UCSR1B = ((1 << RXCIE1) | (1 << RXEN1)); \
-        } while (0)
-#    define PS2_USART_RX_POLL_ON() \
-        do {                       \
-            UCSR1B = (1 << RXEN1); \
-        } while (0)
-#    define PS2_USART_OFF()                           \
-        do {                                          \
-            UCSR1C = 0;                               \
-            UCSR1B &= ~((1 << RXEN1) | (1 << TXEN1)); \
-        } while (0)
-#    define PS2_USART_RX_READY (UCSR1A & (1 << RXC1))
-#    define PS2_USART_RX_DATA UDR1
-#    define PS2_USART_ERROR (UCSR1A & ((1 << FE1) | (1 << DOR1) | (1 << UPE1)))
-#    define PS2_USART_RX_VECT USART1_RX_vect
+/* synchronous, odd parity, 1-bit stop, 8-bit data, sample at falling edge */
+/* set DDR of CLOCK as input to be slave */
+#define PS2_USART_INIT() do {   \
+    PS2_CLOCK_DDR &= ~(1<<PS2_CLOCK_BIT);   \
+    PS2_DATA_DDR &= ~(1<<PS2_DATA_BIT);     \
+    UCSR1C = ((1 << UMSEL10) |  \
+              (3 << UPM10)   |  \
+              (0 << USBS1)   |  \
+              (3 << UCSZ10)  |  \
+              (0 << UCPOL1));   \
+    UCSR1A = 0;                 \
+    UBRR1H = 0;                 \
+    UBRR1L = 0;                 \
+} while (0)
+#define PS2_USART_RX_INT_ON() do {  \
+    UCSR1B = ((1 << RXCIE1) |       \
+              (1 << RXEN1));        \
+} while (0)
+#define PS2_USART_RX_POLL_ON() do { \
+    UCSR1B = (1 << RXEN1);          \
+} while (0)
+#define PS2_USART_OFF() do {    \
+    UCSR1C = 0;                 \
+    UCSR1B &= ~((1 << RXEN1) |  \
+                (1 << TXEN1));  \
+} while (0)
+#define PS2_USART_RX_READY      (UCSR1A & (1<<RXC1))
+#define PS2_USART_RX_DATA       UDR1
+#define PS2_USART_ERROR         (UCSR1A & ((1<<FE1) | (1<<DOR1) | (1<<UPE1)))
+#define PS2_USART_RX_VECT       USART1_RX_vect
 #endif
 
 #define AUDIO_PIN_ALT B7
--- rules.mk
+++ rules.mk
@@ -3,7 +3,6 @@ F_CPU = 16000000
 ARCH = AVR8
 F_USB = $(F_CPU)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
-
 BOOTLOADER = halfkay
 
 # Build Options
@@ -13,17 +12,18 @@ BOOTLOADER = halfkay
 BOOTMAGIC_ENABLE = lite		# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no		# Mouse keys
 EXTRAKEY_ENABLE = yes		# Audio control and System control
-CONSOLE_ENABLE = no		    # Console for debug
+CONSOLE_ENABLE = no		# Console for debug
 COMMAND_ENABLE = no			# Commands for debug and configuration
 NKRO_ENABLE = no			# Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no		# Enable keyboard backlight functionality
 MIDI_ENABLE = no			# MIDI controls
-AUDIO_ENABLE = no			# Audio output on port C6
+AUDIO_ENABLE = yes			# Audio output on port C6
 UNICODE_ENABLE = no			# Unicode
 BLUETOOTH_ENABLE = no		# Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no		# Enable WS2812 RGB underlight.
-
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no		# Breathing sleep LED during USB suspend
 
-FIRMWARE_FORMAT = hex
+# PS/2 Mouse
+PS2_MOUSE_ENABLE = yes	# Ports used D2 & D5
+PS2_USE_USART = yes
